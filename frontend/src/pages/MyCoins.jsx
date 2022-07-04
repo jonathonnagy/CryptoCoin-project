b8a866eb5f9f2b884798ff5334937f3474da86ca
Jonathon Nagy <nagy.jon93@gmail.com>
2022-07-04 22:07:12 +0200

    error if empty

--- frontend/src/pages/MyCoins.jsx
+++ frontend/src/pages/MyCoins.jsx
@@ -3,7 +3,7 @@ import CoinCard from "../components/CoinCard";
 import http from "axios";
 import { useAuth } from "../providers/auth";
 import MyCoinCard from "../components/MyCoinCard";
-import '../pages/styles/MyCoins.css'
+import "../pages/styles/MyCoins.css";
 
 const MyCoins = () => {
   const { user, reloadPage } = useAuth();
@@ -25,25 +25,27 @@ const MyCoins = () => {
 
   const coinId = savedCoinsId.map((elem) => elem.id);
 
-  
   useEffect(() => {
     getSavedCoinsDB();
   }, [reloadPage]);
-  
 
   // console.log(savedCoinData);
   return (
     <>
       <div className="wrapper-bg">
-      <h1>MyCoins Collection</h1>
+        <h1>MyCoins Collection</h1>
         <div className="top-wrapper top-wrapper-mycoin">
           <div className="card-container">
-            {coinId.map((id, index) => (
-              <MyCoinCard key={index} id={id} handler={setState} />
-              ))}
+            {coinId.length < 1 ? (
+              <h2>No saved coin!</h2>
+            ) : (
+              coinId.map((id, index) => (
+                <MyCoinCard key={index} id={id} handler={setState} />
+              ))
+            )}
           </div>
         </div>
-              <div className="space-cat-img"></div>
+        <div className="space-cat-img"></div>
       </div>
     </>
   );
