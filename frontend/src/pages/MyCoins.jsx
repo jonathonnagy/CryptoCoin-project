0618e179a4bb2aed6f198fd4233f0b8124fbf37b
Jonathon Nagy <nagy.jon93@gmail.com>
2022-07-04 15:01:54 +0200

    profile inputs and country search with npm

--- frontend/src/pages/MyCoins.jsx
+++ frontend/src/pages/MyCoins.jsx
@@ -3,7 +3,7 @@ import CoinCard from "../components/CoinCard";
 import http from "axios";
 import { useAuth } from "../providers/auth";
 import MyCoinCard from "../components/MyCoinCard";
-import '../index.css'
+import '../pages/styles/MyCoins.css'
 
 const MyCoins = () => {
   const { user, reloadPage } = useAuth();
@@ -34,15 +34,16 @@ const MyCoins = () => {
   // console.log(savedCoinData);
   return (
     <>
-      <div>MyCoins</div>
-      <div className="bottom">
-        <div className="bottom-wrapper">
+      <div className="wrapper-bg">
+      <h1>MyCoins Collection</h1>
+        <div className="top-wrapper top-wrapper-mycoin">
           <div className="card-container">
             {coinId.map((id, index) => (
-					<MyCoinCard key={index} id={id} handler={setState} />
-					))}
+              <MyCoinCard key={index} id={id} handler={setState} />
+              ))}
           </div>
         </div>
+              <div className="space-cat-img"></div>
       </div>
     </>
   );
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState, useMemo } from "react";
+import Select from "react-select";
+import countryList from "react-select-country-list";
 
 const Profile = () => {
+  const [value, setValue] = useState("");
+  const options = useMemo(() => countryList().getData(), []);
+
+  const changeHandler = (value) => {
+    setValue(value);
+  };
   return (
-	<>
-		<div>Profile</div>
-		<p>First name: </p>
-		<p>Last name: </p>
-		<p>Birthdate: </p>
-		<p>Country: </p>
+    <>
+      <div className="wrapper-bg">
+        <h1>Profile</h1>
 
-	</>
-  )
-}
+        <p>First name: </p>
+        <input type="text" id="first-name" placeholder="First name" />
+        <p>Last name: </p>
+        <input type="text" id="last-name" placeholder="Last name" />
+        <p>Birthdate: </p>
+        <input type="date" id="birthdate" />
+        <p>Country: </p>
+        <Select options={options} value={value} onChange={changeHandler} />
+      </div>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
